08b6bc5281d2d483a2873cdaf7a21c9e068e5f46
LegenDu <du.leg@husky.neu.edu>
2020-04-09 15:54:33 -0700

    Modified server URL to heroku server

--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-// const URL = 'https://wbdv-sp20-server-node-a9.herokuapp.com'
-const URL = 'http://localhost:3000'
+const URL = 'https://wbdv-sp20-server-node-a9.herokuapp.com'
+// const URL = 'http://localhost:3000'
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
